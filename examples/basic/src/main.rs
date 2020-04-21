22086d78bb3999dcb1a2806c02be17111c134195
Richard <Richard>
2020-04-21 16:55:45 -0700

    first commit

---
+++ examples/basic/src/main.rs
@@ -0,0 +1,23 @@
+use ukiyoe::*;
+
+fn main() {
+    let mut window = Window::new();
+    let mut b1 = Button::new("a");
+    let b2 = Button::new("b");
+    let mut b3 = Button::new("c");
+    let b4 = Button::new("d");
+    let b5 = Button::new("e");
+    b1.add_child(Box::new(b2));
+    b3.add_child(Box::new(b4));
+    b3.add_child(Box::new(b5));
+    b1.add_child(Box::new(b3));
+    window.set_content(Box::new(b1));
+    loop {
+        let event = window.next_event();
+        match event {
+            WindowEvent::Exit => return,
+            _ => {},
+        }
+        window.render();
+    }
+}
