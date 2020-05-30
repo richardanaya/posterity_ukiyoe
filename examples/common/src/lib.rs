e2223777466c98a09cff944082a89c7b712cfe34
Richard <Richard>
2020-05-30 08:05:47 -0700

    adding curses

--- examples/common/src/lib.rs
+++ examples/common/src/lib.rs
@@ -1,22 +1,61 @@
+pub use pancurses::Input;
+use pancurses::{curs_set, endwin, initscr, noecho, Window};
 use ukiyoe::*;
 
-pub struct SillyConsoleRenderer {
-
-}
+pub struct SillyConsoleRenderer {}
 
 impl SillyConsoleRenderer {
-	pub fn new() -> Self {
-		SillyConsoleRenderer {
-		}
-	}
+    pub fn new() -> Self {
+        SillyConsoleRenderer {}
+    }
 }
 
 impl Renderer for SillyConsoleRenderer {
-	fn draw_rectangle(&self, r: Rect) {
-		println!("look ma! no hands!");
-		println!("{:?}", r.position.x );
-	}
-	fn get_dimensions(&self) -> Size {
-		Size { width: 100.0, height: 100.0 }
-	}
+    fn draw_rectangle(&self, r: Rect) {
+        println!("look ma! no hands!");
+        println!("{:?}", r.position.x);
+    }
+    fn get_dimensions(&self) -> Size {
+        Size {
+            width: 100.0,
+            height: 100.0,
+        }
+    }
+}
+
+pub struct CursesRenderer {
+    window: Window,
+}
+
+impl Drop for CursesRenderer {
+    fn drop(&mut self) {
+        endwin();
+    }
+}
+
+impl CursesRenderer {
+    pub fn new() -> Self {
+        let w = initscr();
+        noecho();
+        curs_set(0);
+        CursesRenderer { window: w }
+    }
+
+    pub fn getch(&self) -> Option<Input> {
+        self.window.getch()
+    }
+}
+
+impl Renderer for CursesRenderer {
+    fn draw_rectangle(&self, r: Rect) {
+        self.window.printw("Hello Rust");
+        self.window.refresh();
+        //window.getch();
+    }
+    fn get_dimensions(&self) -> Size {
+        Size {
+            width: 100.0,
+            height: 100.0,
+        }
+    }
 }
--- examples/rectangler/src/main.rs
+++ examples/rectangler/src/main.rs
@@ -1,8 +1,17 @@
-use ukiyoe::*;
 use common::*;
+use ukiyoe::*;
 
 fn main() {
-    let renderer = SillyConsoleRenderer::new();
-    let Size{width:w,height:h} = renderer.get_dimensions();
-    renderer.draw_rectangle(Rect::new(0.0,0.0,w/2.0,h/2.0));
+    let renderer = CursesRenderer::new();
+    let Size {
+        width: w,
+        height: h,
+    } = renderer.get_dimensions();
+    renderer.draw_rectangle(Rect::new(0.0, 0.0, w / 2.0, h / 2.0));
+    loop {
+        // if escape pressed
+        if renderer.getch() == Some(Input::Character('\u{1b}')) {
+            break;
+        }
+    }
 }
