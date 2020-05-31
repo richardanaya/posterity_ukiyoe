b2dcc534cfec7872a7b9b59adf491ead6fbd2ac1
Darryl T. Agostinelli <dagostinelli@gmail.com>
2020-05-31 11:01:37 -0500

    initial checkin of sample glfw

---
+++ examples/glfw_opengl4/src/main.rs
@@ -0,0 +1,33 @@
+extern crate glfw;
+
+use glfw::{Action, Context, Key};
+
+fn main() {
+   let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+
+    // Create a windowed mode window and its OpenGL context
+    let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
+        .expect("Failed to create GLFW window.");
+
+    // Make the window's context current
+    window.make_current();
+    window.set_key_polling(true);
+
+    // Loop until the user closes the window
+    while !window.should_close() {
+        // Swap front and back buffers
+        window.swap_buffers();
+
+        // Poll for and process events
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            println!("{:?}", event);
+            match event {
+                glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                    window.set_should_close(true)
+                },
+                _ => {},
+            }
+        }
+    }
+}
